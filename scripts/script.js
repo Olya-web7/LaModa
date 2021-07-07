ff1a9db090350ee801979f7e4456989478b8c937
“Olha <shyshova24@gmail.com>
2021-07-07 16:42:33 +0300

    some more edit

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
+let hash = location.hash.substring(1);
+
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Your city';
 
 headerCityButton.addEventListener('click', () => {
@@ -60,10 +62,14 @@ const getData = async() => {
     }
 };
 
-const getGoods = (callBack) => {
+const getGoods = (callback, value) => {
     getData()
         .then(data => {
-            callBack(data);
+            if (value) {
+                callback(data.filter(item => item.category === value));
+            } else {
+                callback(data);
+            }
         })
         .catch(err => {
             console.log(err);
@@ -88,21 +94,24 @@ try {
         throw 'This is not a goods page'
     }
 
-    const createCard = data => {
-        const li = document.createElement('li');
+    const createCard = ({ id, preview, cost, brand, name, sizes }) => {
 
-        li.classList.add('goods__item');
+            const li = document.createElement('li');
 
-        li.innerHTML = `
+            li.classList.add('goods__item');
+
+            li.innerHTML = `
         <article class="good">
-        <a class="good__link-img" href="card-good.html#id56454">
-            <img class="good__img" src="goods-image/MP002XG0147V_11514976_1_v1.jpg" alt="">
+        <a class="good__link-img" href="card-good.html#${id}">
+            <img class="good__img" src="goods-image/${preview}" alt="">
         </a>
         <div class="good__description">
-            <p class="good__price">2890 &#8381;</p>
-            <h3 class="good__title">Eazyway <span class="good__title__grey">/ Тайтсы</span></h3>
-            <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">40 42 44 46</span></p>
-            <a class="good__link" href="card-good.html#id56454">Подробнее</a>
+            <p class="good__price">${cost} &#8381;</p>
+            <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+            ${sizes ? 
+                `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+            ' '}
+            <a class="good__link" href="card-good.html#${id}">Подробнее</a>
         </div>
     </article>
         `;
@@ -111,10 +120,20 @@ try {
     };
 
     const renderGoodsList = data => {
-        console.log(data);
+        goodsList.textContent = '';
+
+        data.forEach(item => {
+            const card = createCard(item);
+            goodsList.append(card);
+        });
     };
 
-    getGoods(renderGoodsList);
+    window.addEventListener('hashchange', () => {
+        hash = location.hash.substring(1);
+        getGoods(renderGoodsList, hash);
+    })
+
+    getGoods(renderGoodsList, hash);
 
 } catch (err) {
     console.warn(err)
