75ee1be6cae76611ce7e728c0aaede86af29f0b3
“Olha <shyshova24@gmail.com>
2021-07-07 13:24:51 +0300

    edit

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,29 @@ headerCityButton.addEventListener('click', () => {
     headerCityButton.textContent = city;
     localStorage.setItem('lomoda-location', city);
 })
+
+//modal
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderCart.addEventListener('click', () => {
+    cartModalOpen();
+});
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
+});
